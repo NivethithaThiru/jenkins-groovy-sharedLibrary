ee13297b3c987f0bcb02c38464dfd05b5e516dea
NivethithaThiru <59823605+NivethithaThiru@users.noreply.github.com>
2021-07-22 02:27:49 +0530

    Update productReadFile.groovy

--- vars/productReadFile.groovy
+++ vars/productReadFile.groovy
@@ -1,10 +1,10 @@
+#!/usr/bin/env groovy
+
 def readFile()
 {
-    new File("C:/Users/nivir/Desktop/Groovy/product.csv").eachLine {  
+    new File("https://github.com/NivethithaThiru/jenkins-groovy-sharedLibrary/blob/master/product.csv").eachLine {  
          line -> println "$line"; 
     }
 } 
-static void main(String[] args) { 
-     readFile();
-} 
+
    
