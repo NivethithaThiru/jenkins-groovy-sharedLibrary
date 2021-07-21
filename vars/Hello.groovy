615a1b5eb53f75b280c854197f0f9af763e51045
NivethithaThiru <59823605+NivethithaThiru@users.noreply.github.com>
2021-07-22 02:10:29 +0530

    Updated Hello.groovy

--- vars/Hello.groovy
+++ vars/Hello.groovy
@@ -1,7 +1,9 @@
+#!/usr/bin/env groovy
+
 def call(String name = 'human') {
     println("Hello, ${name}.");
 }
 
-static void main(args) {
-    call("Nivethitha Thirumurugan");
-}
+//static void main(args) {
+    //call("Nivethitha Thirumurugan");
+//}
