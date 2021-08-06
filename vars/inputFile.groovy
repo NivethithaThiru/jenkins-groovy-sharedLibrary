33069bb3fd8df60a0a2d0e2b09de08fcdb1b72e3
NivethithaThiru <59823605+NivethithaThiru@users.noreply.github.com>
2021-08-06 14:09:04 +0530

    Update inputFile.groovy

--- vars/inputFile.groovy
+++ vars/inputFile.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def fileRead(String filename = "product.csv") {
+    println("Displaying File Contents");
     def filePath = readFile "${filename}" 
     def lines = filePath.readLines() 
     for (line in lines) { 
