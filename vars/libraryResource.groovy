deace6ee81ad79452da58589b3e5417f62df578f
NivethithaThiru <59823605+NivethithaThiru@users.noreply.github.com>
2021-08-12 01:13:30 +0530

    Update libraryResource.groovy

--- vars/libraryResource.groovy
+++ vars/libraryResource.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def createFile(String fileName) {
+def call(String fileName) {
     def fileContent = libraryResource "csvfiles/${fileName}"
     writeFile file: "product1.csv", text: fileContent
 } 
